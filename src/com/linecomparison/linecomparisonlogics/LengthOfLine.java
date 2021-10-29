5bdd04b0968e086c25b80e378e28775915985637
musai7 <musaishankar307@gmail.com>
2021-10-29 20:27:03 +0530

    modified lenght of a line logic

--- src/com/linecomparison/linecomparisonlogics/LengthOfLine.java
+++ src/com/linecomparison/linecomparisonlogics/LengthOfLine.java
@@ -9,6 +9,6 @@ public static void main(String[] args) {
 	y1=sc.nextInt();
 	x2=sc.nextInt();
 	y2=sc.nextInt();
-	System.out.println(Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+	System.out.println(Math.sqrt(Math.pow((x2-x1),2)+(y2-y1)*(y2-y1)));
 }
 }
